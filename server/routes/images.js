a706051ff2444b795d0bf8f87db9c20ea11c6f41
RuoyuLi97 <li.ruoyu@northeastern.edu>
2025-06-17 11:48:00 -0700

    update images routes

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const {upload, uploadImage, deleteImage, updateImage, getImages} = require('../controllers/imagesController');
-const s3 = require('../utils/s3Client');
 
 const router = express.Router();
 
