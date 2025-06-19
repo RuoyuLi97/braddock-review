079d17d42fbf0648f9f2c768cdcbd76152b8b22e
RuoyuLi97 <li.ruoyu@northeastern.edu>
2025-06-19 15:21:00 -0700

    update error handling for other methods

--- server/routes/healthCheck.js
+++ server/routes/healthCheck.js
@@ -2,7 +2,26 @@ const express = require('express');
 const router = express.Router();
 const healthCheckController = require('../controllers/healthCheckController');
 
-router.get('/', healthCheckController.getHealthStatus);
-router.get('/connection', healthCheckController.getConnectionHealthStatus);
+router.route('/')
+        .get(healthCheckController.getHealthStatus)
+        .all((req, res) => {
+            res.set('Allow', 'GET');
+            res.status(405).json({
+                error: 'Method not allowed!',
+                message: `${req.method} method is not supported on this endpoint!`,
+                allowedMethods: ['GET']
+            });
+        });
+
+router.route('/connection')
+        .get(healthCheckController.getConnectionHealthStatus)
+        .all((req, res) => {
+            res.set('Allow', 'GET');
+            res.status(405).json({
+                error: 'Method not allowed!',
+                message: `${req.method} method is not supported on this endpoint!`,
+                allowedMethods: ['GET']
+            });
+        });
 
 module.exports = router
